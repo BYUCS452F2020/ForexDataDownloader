a60c71c04174501972f098edf618295e71630fe0
Ethan Pedersen <ethanp55@gmail.com>
2020-09-16 16:39:58 -0600

    Add doc strings and comments to the user dao file.

--- UnitTests/DAOTests/user_dao_test.py
+++ UnitTests/DAOTests/user_dao_test.py
@@ -1,33 +1,50 @@
 import unittest
 from DAO.user_dao import UserDao
 
-
 # TODO: add more tests for failures, etc.
 class UserDaoTest(unittest.TestCase):
     def setUp(self) -> None:
         self.user_dao = UserDao()
 
+    def test_insert_new_user_username_fail(self):
+        self.user_dao._create_user_table()
+
+        success, error_message = self.user_dao.insert_new_user('', 'Billy', 'Bob', 'bob@gmail.com', 'basic')
+
+        self.assertEqual(error_message, 'Invalid new user parameters')
+
     def test_insert_new_user_name_fail(self):
-        success, error_message = self.user_dao.insert_new_user('Billy', '', 'bob@gmail.com', 'basic')
+        self.user_dao._create_user_table()
+
+        success, error_message = self.user_dao.insert_new_user('billybob123', 'Billy', '', 'bob@gmail.com', 'basic')
 
         self.assertEqual(error_message, 'Invalid new user parameters')
 
     def test_insert_new_user_email_fail(self):
-        success, error_message = self.user_dao.insert_new_user('Billy', 'Bob', '', 'basic')
+        self.user_dao._create_user_table()
+
+        success, error_message = self.user_dao.insert_new_user('billybob123', 'Billy', 'Bob', '', 'basic')
 
         self.assertEqual(error_message, 'Invalid new user parameters')
 
     def test_insert_new_user_subscription_type_fail(self):
-        success, error_message = self.user_dao.insert_new_user('Billy', 'Bob', '', 'basicc')
+        self.user_dao._create_user_table()
+
+        success, error_message = self.user_dao.insert_new_user('billybob123', 'Billy', 'Bob', '', 'basicc')
 
         self.assertEqual(error_message, 'Invalid new user parameters')
 
     def test_insert_new_user_success(self):
-        success, error_message = self.user_dao.insert_new_user('Billy', 'Bob', 'bob@gmail.com', 'basic')
+        self.user_dao._create_user_table()
+
+        success, error_message = self.user_dao.insert_new_user('billybob123', 'Billy', 'Bob', 'bob@gmail.com', 'basic')
 
         self.assertTrue(success)
 
     def test_get_user_by_name_success(self):
+        self.user_dao._create_user_table()
+
+        success, error_message = self.user_dao.insert_new_user('billybob123', 'Billy', 'Bob', 'bob@gmail.com', 'basic')
         user, error_message = self.user_dao.get_user_by_name('Billy', 'Bob')
 
         self.assertIsNotNone(user)
