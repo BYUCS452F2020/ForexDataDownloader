7f3635e506252c81d3ab60a816bb22860e75bd81
Ethan Pedersen <ethanp55@gmail.com>
2020-09-28 19:34:09 -0600

    Add a few more functions to the currency pair service.  Create a service facade class.  More implementation needed once the DAOs and services are complete.

--- Services/CurrencyPair/currency_pair_service.py
+++ Services/CurrencyPair/currency_pair_service.py
@@ -7,12 +7,20 @@ class CurrencyPairService:
         self.pair_followed_dao = None  # TODO: get the actual pair followed dao once it's implemented
         self.followed_pairs_left_dao = None  # TODO: get the actual followed pairs left dao once it's implemented
 
+    # TODO: implement and add docs when currency pairs dao is implemented
+    def get_available_currency_pairs(self):
+        pass
+
     # TODO: implement and add docs when pair followed dao is implemented
     def get_pairs_followed_for_user(self, user_id):
         pass
 
     # TODO: implement and add docs when pair followed dao is implemented
     def update_pairs_followed_for_user(self, user_id, currency_pair_id):
+        pass
+
+    # TODO: implement and add docs when followed pairs left dao is implemented
+    def get_followed_pairs_left_for_user(self, user_id):
         pass
 
     # TODO: implement and add docs when followed pairs left dao is implemented
