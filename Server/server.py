807d982f7c1235ceac97c985f4e029c66f26bb48
Ethan Pedersen <ethanp55@gmail.com>
2020-10-19 19:16:35 -0600

    Wire up the server.  Backend is basically done, though it would be good to add more tests (especially testing for edge cases and failures) and documentation.

--- Server/server.py
+++ Server/server.py
@@ -3,7 +3,9 @@ from Services.service_facade import ServiceFacade
 import json
 
 
-# TODO: implement and add docs/comments
+# TODO: add checks on each function for the parameters that are required (ex: when creating a new user, we need the
+#  username, first name, etc., so make sure all of those are passed in)
+# TODO: add docs and tests
 class Handler(BaseHTTPRequestHandler):
     def __init__(self, request, client_address, server):
         self.service_facade = ServiceFacade()
@@ -20,23 +22,28 @@ class Handler(BaseHTTPRequestHandler):
         return request_body
 
     def _send_response(self, code):
-        # TODO: code is 200 (success) or 404/other failure code
         self.send_response(code)
-        # TODO: do we need this?
+        # Do we need this?
         # self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-    # TODO: check for errors and send 404 responses accordingly
-    # TODO: add docs/comments
-    # TODO: double check that everything aligns properly with the service facade (items that are returned, input parameters, etc.)
-    # TODO: test everything
     def do_GET(self):
         if self.path == '/getAvailablePairs':
-            pairs = self.service_facade.get_available_currency_pairs()
+            available_pairs, error_message = self.service_facade.get_available_currency_pairs()
+
+            if error_message is not None:
+                self._send_response(404)
+                self.wfile.write(error_message.encode())
+                return
+
+            pairs = []
+
+            for pair in available_pairs:
+                pairs.append(pair[0])
+
             pairs_json = json.dumps(pairs)
 
             self._send_response(200)
-
             self.wfile.write(pairs_json.encode())
 
         elif self.path == '/getPairsFollowed':
@@ -46,11 +53,21 @@ class Handler(BaseHTTPRequestHandler):
 
             user_id = request_body['user_id']
 
-            followed_pairs = self.service_facade.get_pairs_followed_for_user(user_id)
+            followed_pairs_for_user, error_message = self.service_facade.get_pairs_followed_for_user(user_id)
+
+            if error_message is not None:
+                self._send_response(404)
+                self.wfile.write(error_message.encode())
+                return
+
+            followed_pairs = []
+
+            for pair in followed_pairs_for_user:
+                followed_pairs.append(pair[0])
+
             followed_pairs_json = json.dumps(followed_pairs)
 
             self._send_response(200)
-
             self.wfile.write(followed_pairs_json.encode())
 
         elif self.path == '/getFollowedPairsLeft':
@@ -60,11 +77,16 @@ class Handler(BaseHTTPRequestHandler):
 
             user_id = request_body['user_id']
 
-            pairs_left = self.service_facade.get_followed_pairs_left_for_user(user_id)
-            pairs_left_json = json.dumps(pairs_left)
+            pairs_left, error_message = self.service_facade.get_followed_pairs_left_for_user(user_id)
+
+            if error_message is not None:
+                self._send_response(404)
+                self.wfile.write(error_message.encode())
+                return
+
+            pairs_left_json = json.dumps(pairs_left[0])
 
             self._send_response(200)
-
             self.wfile.write(pairs_left_json.encode())
 
         elif self.path == '/getHistoricalData':
@@ -91,13 +113,27 @@ class Handler(BaseHTTPRequestHandler):
                 candles_json = candles.to_json(orient='records')
 
                 self._send_response(200)
-
                 self.wfile.write(candles_json.encode())
 
-    # TODO: check for errors and send 404 responses accordingly
-    # TODO: add docs/comments
-    # TODO: double check that everything aligns properly with the service facade (items that are returned, input parameters, etc.)
-    # TODO: test everything
+        elif self.path == '/getMonthlyBill':
+            content_len = int(self.headers['Content-Length'])
+            post_body = self.rfile.read(content_len).decode('utf-8')
+            request_body = self._format_request_body(post_body)
+
+            user_id = request_body['user_id']
+
+            monthly_bill, error_message = self.service_facade.get_monthly_bill(user_id)
+
+            if error_message is not None:
+                self._send_response(404)
+                self.wfile.write(error_message.encode())
+                return
+
+            monthly_bill_json = json.dumps(monthly_bill[0])
+
+            self._send_response(200)
+            self.wfile.write(monthly_bill_json.encode())
+
     def do_POST(self):
         if self.path == '/updatePairsFollowed':
             content_len = int(self.headers['Content-Length'])
@@ -107,7 +143,12 @@ class Handler(BaseHTTPRequestHandler):
             user_id = request_body['user_id']
             currency_pair_name = request_body['currency_pair_name']
 
-            self.service_facade.update_pairs_followed_for_user(user_id, currency_pair_name)
+            message = self.service_facade.update_pairs_followed_for_user(user_id, currency_pair_name)
+
+            if message != 'Successfully updated pairs followed':
+                self._send_response(404)
+                self.wfile.write(message.encode())
+                return
 
             self._send_response(200)
 
@@ -119,7 +160,12 @@ class Handler(BaseHTTPRequestHandler):
             user_id = request_body['user_id']
             subscription_type = request_body['subscription_type']
 
-            self.service_facade.update_subscription(user_id, subscription_type)
+            message = self.service_facade.update_subscription(user_id, subscription_type)
+
+            if message != 'Updated subscription':
+                self._send_response(404)
+                self.wfile.write(message.encode())
+                return
 
             self._send_response(200)
 
@@ -131,13 +177,20 @@ class Handler(BaseHTTPRequestHandler):
             username = request_body['username']
             first_name = request_body['first_name']
             last_name = request_body['last_name']
+            password = request_body['password']
             subscription_type = request_body['subscription_type']
 
-            user_id = self.service_facade.create_user(username, first_name, last_name, subscription_type)
+            user_id, error_message = self.service_facade.create_user(username, first_name, last_name, password,
+                                                                     subscription_type)
+
+            if error_message is not None:
+                self._send_response(404)
+                self.wfile.write(error_message.encode())
+                return
+
             user_id_json = json.dumps(user_id)
 
             self._send_response(200)
-
             self.wfile.write(user_id_json.encode())
 
         elif self.path == '/login':
@@ -148,11 +201,16 @@ class Handler(BaseHTTPRequestHandler):
             username = request_body['username']
             password = request_body['password']
 
-            user_id = self.service_facade.login(username, password)
-            user_id_json = json.dumps(user_id)
+            user_id, error_message = self.service_facade.login(username, password)
+
+            if error_message is not None:
+                self._send_response(404)
+                self.wfile.write(error_message.encode())
+                return
+
+            user_id_json = json.dumps(user_id[0])
 
             self._send_response(200)
-
             self.wfile.write(user_id_json.encode())
 
 
--- Server/test.py
+++ Server/test.py
@@ -1,6 +1,8 @@
 import requests as req
 import json
 
+
+# Get some candle data for EUR/USD
 headers = {'Content-Type': 'application/json'}
 data = {'currency_pair': 'EUR_USD', 'time_frame_granularity': 'H1',
         'from_time': '2020-10-05 00:00:00', 'to_time': '2020-10-06 00:00:00'}
@@ -13,4 +15,140 @@ if resp.status_code != 404:
 
 else:
         print(resp.text)
+
+
+# Create a user
+headers = {'Content-Type': 'application/json'}
+data = {'username': 'abc123', 'first_name': 'Billy', 'last_name': 'Bob', 'password': 'a', 'subscription_type': 'Premium'}
+resp = req.post('http://localhost:8080/createUser', headers=headers, data=data)
+
+if resp.status_code != 404:
+        user_id = json.loads(resp.text)
+        print(user_id)
+
+else:
+        print(resp.text)
+
+
+# Create the same user and make sure we get an error message
+headers = {'Content-Type': 'application/json'}
+data = {'username': 'abc123', 'first_name': 'Billy', 'last_name': 'Bob', 'password': 'a', 'subscription_type': 'Premium'}
+resp = req.post('http://localhost:8080/createUser', headers=headers, data=data)
+
+if resp.status_code != 404:
+        user_id = json.loads(resp.text)
+        print(user_id)
+
+else:
+        print(resp.text)
+
+
+# Login and check the user id
+headers = {'Content-Type': 'application/json'}
+data = {'username': 'abc123', 'password': 'a'}
+resp = req.post('http://localhost:8080/login', headers=headers, data=data)
+
+if resp.status_code != 404:
+        user_id_2 = json.loads(resp.text)
+        print(user_id_2 == user_id)
+
+else:
+        print(resp.text)
+
+
+# Get monthly bill
+headers = {'Content-Type': 'application/json'}
+data = {'user_id': user_id}
+resp = req.get('http://localhost:8080/getMonthlyBill', headers=headers, data=data)
+
+if resp.status_code != 404:
+        bill = json.loads(resp.text)
+        print(bill)
+
+else:
+        print(resp.text)
+
+
+# Get the amount of pairs followed left
+headers = {'Content-Type': 'application/json'}
+data = {'user_id': user_id}
+resp = req.get('http://localhost:8080/getFollowedPairsLeft', headers=headers, data=data)
+
+if resp.status_code != 404:
+        pairs_left = json.loads(resp.text)
+        print(pairs_left)
+
+else:
+        print(resp.text)
+
+
+# Change to the basic subscription
+headers = {'Content-Type': 'application/json'}
+data = {'user_id': user_id, 'subscription_type': 'Basic'}
+resp = req.post('http://localhost:8080/updateSubscription', headers=headers, data=data)
+
+if resp.status_code == 404:
+        print(resp.text)
+
+
+# Check to see if the monthly bill changed
+headers = {'Content-Type': 'application/json'}
+data = {'user_id': user_id}
+resp = req.get('http://localhost:8080/getMonthlyBill', headers=headers, data=data)
+
+if resp.status_code != 404:
+        bill = json.loads(resp.text)
+        print(bill)
+
+else:
+        print(resp.text)
+
+
+# Check to see if the pairs followed left changed
+headers = {'Content-Type': 'application/json'}
+data = {'user_id': user_id}
+resp = req.get('http://localhost:8080/getFollowedPairsLeft', headers=headers, data=data)
+
+if resp.status_code != 404:
+        pairs_left = json.loads(resp.text)
+        print(pairs_left)
+
+else:
+        print(resp.text)
+
+
+# Follow a pair
+headers = {'Content-Type': 'application/json'}
+data = {'user_id': user_id, 'currency_pair_name': 'EUR/USD'}
+resp = req.post('http://localhost:8080/updatePairsFollowed', headers=headers, data=data)
+
+if resp.status_code == 404:
+        print(resp.text)
+
+
+# Check to see if the pairs followed left changed
+headers = {'Content-Type': 'application/json'}
+data = {'user_id': user_id}
+resp = req.get('http://localhost:8080/getFollowedPairsLeft', headers=headers, data=data)
+
+if resp.status_code != 404:
+        pairs_left = json.loads(resp.text)
+        print(pairs_left)
+
+else:
+        print(resp.text)
+
+
+# Look at the followed pairs
+headers = {'Content-Type': 'application/json'}
+data = {'user_id': user_id}
+resp = req.get('http://localhost:8080/getPairsFollowed', headers=headers, data=data)
+
+if resp.status_code != 404:
+        pairs_followed = json.loads(resp.text)
+        print(pairs_followed)
+
+else:
+        print(resp.text)
+
 
